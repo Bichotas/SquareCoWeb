6070aee5922d36124c3df71dba6847fefd12b60d
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-03-25 17:53:53 +0000

    Se comentaron varias cosas
    Ghoulghaboy

--- app/src/components/Navigation_Bar/Version_2Navigation.tsx
+++ app/src/components/Navigation_Bar/Version_2Navigation.tsx
@@ -258,12 +258,14 @@ export default function Swibc({ children }) {
                   >
                     <HStack>
                       <Avatar
+                        borderColor={"white"}
+                        borderWidth={"3px"}
                         size={"sm"}
                         src={
                           "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
                         }
                       />
-                      <VStack
+                      {/* <VStack
                         display={{ base: "none", md: "flex" }}
                         alignItems="flex-start"
                         spacing="1px"
@@ -273,7 +275,9 @@ export default function Swibc({ children }) {
                         <Text fontSize="xs" color="gray.600">
                           [TipoDeCuenta]
                         </Text>
-                      </VStack>
+                      </VStack> */}
+
+                      {/* Quitar el chevron si no es necesario */}
                       <Box display={{ base: "none", md: "flex" }}>
                         <FiChevronDown />
                       </Box>
