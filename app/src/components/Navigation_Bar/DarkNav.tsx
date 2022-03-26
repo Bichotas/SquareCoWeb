2c05b64bd7f58d71f791be8a2d1b496f3cfd3ba0
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2022-03-26 16:11:29 -0600

    No se necesitan esos archivos.

--- app/src/components/Navigation_Bar/DarkNav.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DarkNav(props: any) {
-  return <div></div>;
-}
-
-export default DarkNav;
--- app/src/components/Navigation_Bar/LightNav.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LightNav(props: any) {
-  return <div></div>;
-}
-
-export default LightNav;
