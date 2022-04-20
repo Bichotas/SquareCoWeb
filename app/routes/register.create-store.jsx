31fde1f8812b92e5ced4b047ab1999a289c86779
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-04-20 12:30:43 -0500

    Diseñando la pantalla de creacion de tienda

--- app/routes/register.create-store.jsx
+++ app/routes/register.create-store.jsx
@@ -1,29 +1,67 @@
 import React from "react";
-import { getAuth, updateProfile } from "firebase/auth";
-import { useLoaderData } from "@remix-run/react";
-import { adminAuth, checkRole } from "../utils/db.server";
-
-export const loader = async ({ request }) => {
-  const auth = getAuth();
-  const currentUser = auth.currentUser;
-  // console.log("====================================");
-  // console.log(currentUser.email);
-  // console.log("====================================");
-
-  // Llamada para checar el rol
-  // let role = await checkRole(currentUser.email);
-  // console.log(role);
-  let displayName = currentUser.displayName;
-  return displayName;
-};
-
+import { ChakraProvider } from "@chakra-ui/provider";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+  useColorModeValue,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
+import { Form } from "@remix-run/react";
 function register_create_store(props) {
-  // const auth = getAuth();
-  // console.log(auth.currentUser.displayName);
-  const data = useLoaderData();
+  return (
+    <ChakraProvider>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Text>[[Datos]]</Text>
+        </Stack>
 
-  console.log(data);
-  return <div>{data}</div>;
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Form method="post">
+            <VStack gap={1}>
+              <FormControl>
+                <FormLabel></FormLabel>
+                <Input />
+              </FormControl>
+              <FormControl>
+                <FormLabel></FormLabel>
+                <Textarea />
+              </FormControl>
+              <FormControl>
+                <FormLabel></FormLabel>
+                <Select>
+                  <option value={"comprador"}>Comprador</option>
+                  <option value={"vendedor"}>Vendedor</option>
+                </Select>
+              </FormControl>
+              <Button
+                type="submit"
+                color={"white"}
+                bg={"purple.400"}
+                isFullWidth
+                loadingText="Submitting"
+                size={"lg"}
+                _hover={{ bg: "purple.500" }}
+              >
+                Finalizar
+              </Button>
+            </VStack>
+          </Form>
+        </Box>
+      </Stack>
+    </ChakraProvider>
+  );
 }
 
 export default register_create_store;
