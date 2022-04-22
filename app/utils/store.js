5bf19903d975feecc7a5edf312b33c0b44bd15e4
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-04-22 17:30:03 -0500

    Documentos para funciones de las cosas de la tienda

---
+++ app/utils/store.js
@@ -0,0 +1,11 @@
+import { db } from "./db.server";
+
+export async function getStore(id) {
+  const docSnapshot = await db.collection("stores").doc(id).get();
+  if (!docSnapshot.exists) {
+    throw Error("No such a document exists");
+  } else {
+    const post = docSnapshot.data();
+    return post;
+  }
+}
