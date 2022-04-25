65b4111c8ec49f3f5975f30affc8d9f58d1091b4
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-04-25 13:07:15 +0000

    Comentand el codigo de $slug - ruta dinamica para el perfil de la tienda

--- app/routes/stores/$slug.jsx
+++ app/routes/stores/$slug.jsx
@@ -21,45 +21,103 @@ import {
   FormLabel,
   Input,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React from "react";
 
 // Remix things
-import { useLoaderData, useParams } from "@remix-run/react";
-import { checkPropertyStore, getStore, trent } from "../../utils/store";
+import { useLoaderData } from "@remix-run/react";
+import { getStore } from "../../utils/store";
 import { getAuth } from "firebase/auth";
 import { Form } from "@remix-run/react";
 import theme from "../../src/theme";
 // Loader and Action
+//
+// Loader para los datos y condicion
 export const loader = async ({ params }) => {
+  // Se obtienen los datos de la función asincrona y se guardan sus valores.
+  //  -- Este lo que va a guardar va a ser un objeto que tiene las propiedades
   const store = await getStore(params.slug);
+
+  // Identificador de la tienda
+  //    -- Este se guarda para comparar si es el mismo con el usuario que con el
+  //    usuario actual.
   let uid = store.uidStore;
+
+  // Condicion de retorno
+  //
+  //    -- Primero se va a checar si el usuario actual o el usuario que devuelve
+  //    la funcion getAuth().currentUser es igual a null: Esto significa que no
+  //    hay un usuario logeado actualmente, entonces este lo que hace es lo
+  //    siguiente.
   if (getAuth().currentUser == null) {
-    let currentUser = null;
+    // Retornamos un objeto con un subobjeto adentro de el y una propiedad
+    // llamada property.
+    //
+    // El objeto store es el que se guarda anteriormente en una constante
+    // La propiedad "property" tiene como valor un valor booleano. Este nos
+    // ayuda a mostrar ciertos elementos que solo queremos mostrar a cierto tipo
+    // de eventos.
     return { store, property: false };
   } else {
+    // Si es que el valor de getAuth().currentUser tiene algun valor diferente
+    // al de null, entonces vamos a hacer lo siguiente:
+
+    // Guardamos el uid del usuario actual para usuarlo después
     let currentUser = getAuth().currentUser.uid;
+    // Checamos que el usuario current user y el uid del objeto de la tienda
+    // sean iguales, este va a devovler un valor booleano el cual va a ser
+    // utilizado en la condicion
     let sameAccount = currentUser == uid;
+
+    // La condicion para mostrar si hay un usuario registrado y ver si es el
+    // mismo usuario que la cuenta de la tienda
+    // Para esto se usa la condicion de sameAccount que es un valor booleano
     if (sameAccount) {
+      // Si es true, entonces este va a devovler un objeto con store y una
+      // propiedad de property true que va a ser usado para mostrar las cosas de
+      // la tienda para modificar.
       return { store, property: true };
     } else {
+      // Si este e false entonces vamos a devolver un objeto igual al anterior
+      // pero solo con la diferencia de que la propiedad de property false.
       return { store, property: false };
     }
   }
 };
 
+// Action para los fornmularios de cambio y de varias cosas
 export const action = async ({ request }) => {
+  // Del parametro request, vamos a mandar a pedir el metodo de formData para
+  // obtener los datos del formulario
   let formData = await request.formData();
+
+  // Mandamos a llamar los valores del formulario del store y guardamos sus
+  // datos en una variable llamada store. Este va a ser el nombre de la tienda
   let store = formData.get("store");
+  // Valor de la descripcion
   let description = formData.get("description");
+
+  // Descripcion del formularios
   let categoria = formData.get("category");
+  // Action que se va a usuar con los valores
   console.log(store, description, categoria);
+  // Retornamos un valor nulo ya que por el momento no se devuleve nada
   return null;
 };
-function $storeName(props) {
+
+
+// Componente de la tienda
+function $storeName() {
+
+  // Destructuramos los valores de la funcion usuLoaderData()
   const { store, property } = useLoaderData();
-
+  
+  // Destructuramos los metodos para usuarlos en el modal, este se destructuran
+  // dessde la funcion useDisclosure()
+  //
+  // @todo -- Poner el link del moodle y useDisclosure()
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  
+  // Devolvemos un componente 
   return (
     <ChakraProvider theme={theme}>
       <Heading>
@@ -104,6 +162,7 @@ function $storeName(props) {
                     <FormControl isRequired>
                       <FormLabel>Nombre de la tienda</FormLabel>
                       <Input
+                        value={store.nameStore}
                         as={Input}
                         id={"store"}
                         name={"store"}
