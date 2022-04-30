9d97c26d09e4d34559a9649a45b450eb8a7c092f
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-04-30 01:38:10 +0000

    Diseñando parte de configuracion -- Menu
    Faltaría diseñar bien la parte para las tarjetas o los mneus

--- app/routes/configuration/index.jsx
+++ app/routes/configuration/index.jsx
@@ -4,10 +4,26 @@ function Configuration(props) {
   return (
     <ChakraProvider>
       <Stack>
-        <Heading fontSize={["2xl", "3xl", "5xl", "6xl"]} letterSpacing={"2px"}>
+        <Heading
+          fontSize={["2xl", "3xl", "5xl", "6xl"]}
+          letterSpacing={"2px"}
+          textShadow={"4px 4px 2px rgba(0, 0, 0, 0.6)"}
+          color={"white"}
+        >
           Mi cuenta
         </Heading>
         <Box width={"100%"} bg={"gray.300"} height={"4px"} />
+        <Stack gap={10} direction={"row"} justify={"left"}>
+          <Box
+            width={"sm"}
+            bg={"beige"}
+            height={"7em"}
+            borderRadius={"15px"}
+            textAlign={"center"}
+          >
+            Tarjeta
+          </Box>
+        </Stack>
       </Stack>
     </ChakraProvider>
   );
