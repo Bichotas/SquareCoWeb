ab884cc3023757d5f271f704d5e8466fa82dd13e
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-05-01 22:31:16 -0500

    pendejadas

--- app/routes/configuration/file.jsx
+++ app/routes/configuration/file.jsx
@@ -1,45 +1,20 @@
-import { Button } from "@chakra-ui/react";
-import { Form } from "@remix-run/react";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-import { unstable_parseMultipartFormData } from "@remix-run/node";
+import { unstable_parseMultipartFormData as parseMultipartFormData } from "@remix-run/node";
+import { getStorage } from "firebase-admin/storage";
+
+import createFirebaseStorageFileHandler from "remix-firebase-storage-file-handler";
+
 export const action = async ({ request }) => {
-  const storage = getStorage();
-  const refStorage = ref(storage, "users");
-  const uploadHandler = async ({ name, stream, filename }) => {
-    const chunks = [];
-    for await (const chunk of stream) {
-      chunks.push(chunk);
-    }
-
-    const buffer = Buffer.concat(chunks);
-    let bytes = new Uint8Array(buffer);
-    console.log(bytes);
-    let valor = await uploadBytes(refStorage, bytes);
-
-    return valor;
-  };
-  const formData = await unstable_parseMultipartFormData(
+  const formData = await parseMultipartFormData(
     request,
-    uploadHandler
+    createFirebaseStorageFileHandler({
+      // Required: provide a reference to a file
+      file({ filename }) {
+        return getStorage().bucket().file(filename);
+      },
+    })
   );
 
-  // the returned value for the file field is whatever our uploadHandler returns.
-  // Let's imagine we're uploading the avatar to s3,
-  // so our uploadHandler returns the URL.
-  const avatarUrl = formData.get("avatar");
-  console.log(avatarUrl);
-  // success! Redirect to account page
-  return null;
+  const url = formData.get("my-file-input");
+
+  // Do something with the URL!
 };
-
-export default function AvatarUploadRoute() {
-  return (
-    <Form method="post" encType="multipart/form-data">
-      <label htmlFor="avatar-input">Avatar</label>
-      <input id="avatar-input" type="file" name="avatar" />
-      <Button m={4} bg={"dodgerblue"} color={"white"} type={"submit"}>
-        Upload
-      </Button>
-    </Form>
-  );
-}
